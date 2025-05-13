bf40dbb5b7987a309947ee39dd94d2495ea44485
Eduardo Minholi <eduarddo.minholi@gmail.com>
2025-05-12 23:22:24 -0300

    progresso

---
+++ analyzer/target/debug/analyzer.d
@@ -0,0 +1,1 @@
+C:\Users\eduar\OneDrive\Documentos\GitHub\Trab_LP_Rust\analyzer\target\debug\analyzer.exe: C:\Users\eduar\OneDrive\Documentos\GitHub\Trab_LP_Rust\analyzer\src\main.rs
--- linguagem/src/analyzer.rs
+++ linguagem/src/analyzer.rs
@@ -1,4 +1,6 @@
-static program = "
+use std::{collections::HashMap, sync::Mutex};
+
+static PROGRAM: &str = "
 var a
 var a
 func f() {
@@ -11,13 +13,21 @@ func g() {
 }
 ";
 
-fn analyzer(program) {
+lazy_static::lazy_static! {
+    static ref FUNCTION_TABLE: Mutex<HashMap<String, usize>> = Mutex::new(HashMap::new());
+    static ref GLOBAL_SYMBOL_TABLE: Mutex<HashMap<String, usize>> = Mutex::new(HashMap::new());
+    static ref LOCAL_SYMBOL_TABLE: Mutex<Vec<String>> = Mutex::new(Vec::new());
+}
+
+fn analyzer(p: &str) {
     let mut memory_address = 0;
     let mut in_function = false;
 
     for (line_number, line) in p.trim().lines().enumerate() {
-        match line.split() {
-            ("var", name) => 
+        match line.lines() {
+            ("var", name) => {
+
+            }
         }
     }
 }
